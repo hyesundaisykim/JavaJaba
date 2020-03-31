8cd0a37660739ea6e84d96735ff705b375dc6070
sajh78 <redksa0708@gmail.com>
2020-03-31 16:59:49 +0900

    곽설아) deptController 수정

--- src/main/java/com/example/demo/controller/DeptController.java
+++ src/main/java/com/example/demo/controller/DeptController.java
@@ -1,9 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.db.DeptManager;
+import com.example.demo.vo.DeptVo;
+import com.google.gson.Gson;
 
 @RestController
 public class DeptController {
-
+	
+	@RequestMapping(value = "/listDept", produces ="application/json;charset=UTF-8" )
+	public String listDept() {
+		String str ="";
+		List<DeptVo> list = DeptManager.listDept();
+		Gson gson = new Gson();
+		str = gson.toJson(list);
+		return str;
+	}
 	
 }
