4ecca42a4136da8c22c1d7c95846ee4f9b470e6e
shuttlekim <kunkun7762@gmail.com>
2020-03-31 16:41:39 +0900

    영현) pom.xml에 gson 추가/

--- src/main/java/com/example/demo/controller/EmpController.java
+++ src/main/java/com/example/demo/controller/EmpController.java
@@ -1,9 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.vo.EmpVo;
 
 @RestController
 public class EmpController {
 
-	
+	@RequestMapping("/insertEmp")
+	public String insertEmp(EmpVo e) {
+		String str = "";
+		
+		return str;
+	}
 }
