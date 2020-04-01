a484cdc697cddfcad020a491c306fb53de6d8f0d
hyunsung0308 <hyunsung0308@gmail.com>
2020-04-01 10:40:35 +0900

    유현성- 컴포넌트 생성

---
+++ src/main/java/com/example/demo/component/SmtpComponent.java
@@ -0,0 +1,32 @@
+package com.example.demo.component;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SmtpComponent {
+
+	@Bean
+	public JavaMailSenderImpl javaMailSender()
+	{
+		JavaMailSenderImpl r = new JavaMailSenderImpl();
+		r.setHost("smtp.gmail.com");
+		r.setPort(587);
+		r.setUsername("hyunsung0308@gmail.com");
+		r.setPassword("암호");
+		r.setDefaultEncoding("UTF-8");
+		
+		Properties prop = new Properties();
+		prop.put("mail.smtp.starttls.enable", true);
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.ssl.checkserveridentity", true);
+		prop.put("mail.smtp.ssl.trust","*");
+		r.setJavaMailProperties(prop);
+		
+		return r;
+
+	}
+}
