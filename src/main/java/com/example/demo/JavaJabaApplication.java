080f87f0a03370111468c1910ef0e27e491662d9
sajh78 <redksa0708@gmail.com>
2020-04-01 11:24:24 +0900

    곽설아)aopLog 수정

--- src/main/java/com/example/demo/JavaJabaApplication.java
+++ src/main/java/com/example/demo/JavaJabaApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class JavaJabaApplication {
 
 	public static void main(String[] args) {
